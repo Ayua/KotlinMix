346ac5494075bd9e1b4b4ed7119b1602bda0086d
Donn Felker <donn@donnfelker.com>
2016-03-01 10:57:06 -0500

    Clean up imports

--- app/src/main/kotlin/com/donnfelker/kotlinmix/EditFragment.kt
+++ app/src/main/kotlin/com/donnfelker/kotlinmix/EditFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.TextView
 import com.donnfelker.kotlinmix.models.Todo
 import io.realm.Realm
 import org.jetbrains.anko.*
